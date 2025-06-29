44fee13deb759e6aceeabf8b79fd9762c16179f7
Archit Singhania <56683524+archit-singhania@users.noreply.github.com>
2025-06-29 19:10:59 +0530

    PORT LISTEN CHANGES

--- backend/index.js
+++ backend/index.js
@@ -129,4 +129,8 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});;
